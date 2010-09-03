b9932d1f1956636d8a6f01c9a01ed9e6cf24299b
Russell Kallman <russell@urbanshake.com.au>
2010-09-03 12:17:49 +1000

    initial commi

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'd8597bd05a6bb2d466a0f6bd29d8f5e56e4e09853f1fcf235df5c279774561bb5ef5b444253397786401ac7ddc4f50899a9c6bf2028109cb2ce43296f087ff6a'
